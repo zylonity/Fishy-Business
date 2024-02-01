43a284b5aed11506f0eb637bf2335a8c438b811e
zylo_dev <kkhaleelkk505@gmail.com>
2024-02-01 05:46:12 +0000

    Updated boundaries and added basic camera movement

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,19 +5,21 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     public GameObject fishingLine;
+    GameObject camera;
     private LineRenderer lineRenderer;
     [SerializeField]
     float fishingLineSpeed = 1f;
     void Start()
     {
         lineRenderer = fishingLine.GetComponent<LineRenderer>();
+        camera = Camera.main.gameObject;
     }
 
     // Update is called once per frame
     void Update()
     {
     //Move player on Mouse X axis, convert mouse to world point
-    if(Input.mousePosition.x > 50 && Input.mousePosition.x < Screen.width){
+    if(Input.mousePosition.x > 150 && Input.mousePosition.x < Screen.width){
         gameObject.transform.position = new Vector3(Camera.main.ScreenToWorldPoint(Input.mousePosition).x, gameObject.transform.position.y, gameObject.transform.position.z);
     }
 
@@ -27,12 +29,21 @@ public class Player : MonoBehaviour
 
     if(Input.GetMouseButton(0)){
         lineRenderer.SetPosition(0, new Vector3(linePos.x, linePos.y - (fishingLineSpeed * Time.deltaTime), linePos.z));
+        //Move Camera
+        if(linePos.y < -4){
+            camera.transform.position = new Vector3(camera.transform.position.x, camera.transform.position.y - (fishingLineSpeed * Time.deltaTime), camera.transform.position.z);
+        }
     }
     else{
         if(linePos.y < 0){
             lineRenderer.SetPosition(0, new Vector3(linePos.x, linePos.y + (fishingLineSpeed * Time.deltaTime), linePos.z));
         }
+        if(camera.transform.position.y < 0){
+            camera.transform.position = new Vector3(camera.transform.position.x, camera.transform.position.y + (fishingLineSpeed * Time.deltaTime), camera.transform.position.z);
+        }
     }
+
+
 
     }
 }
