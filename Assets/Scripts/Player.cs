1b609fb6359e0d95cfae9306011cf3381ddebd73
zylo_dev <kkhaleelkk505@gmail.com>
2024-02-01 07:25:17 +0000

    Added raycast to fishing line and detect fish

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -12,68 +12,72 @@ public class Player : MonoBehaviour
     [SerializeField]
     float cameraCatchUpSpeed = 1.2f;
 
-    RaycastHit hit;
+    Vector3 startPos, linePos;
     void Start()
     {
+
         lineRenderer = fishingLine.GetComponent<LineRenderer>();
         camera = Camera.main.gameObject;
     }
 
-    // Update is called once per frame
+
     void Update()
     {
-    //Move player on Mouse X axis, convert mouse to world point
-    if(Input.mousePosition.x > 150 && Input.mousePosition.x < Screen.width){
-        gameObject.transform.position = new Vector3(Camera.main.ScreenToWorldPoint(Input.mousePosition).x, gameObject.transform.position.y, gameObject.transform.position.z);
-    }
+        //Move player on Mouse X axis, convert mouse to world point
+        if(Input.mousePosition.x > 150 && Input.mousePosition.x < Screen.width){
+            gameObject.transform.position = new Vector3(Camera.main.ScreenToWorldPoint(Input.mousePosition).x, gameObject.transform.position.y, gameObject.transform.position.z);
+        }
 
-    //Move Fishing line
-    Vector3 linePos = lineRenderer.GetPosition(0);
+        //Move Fishing line
+        startPos = lineRenderer.GetPosition(0);
+        linePos = lineRenderer.GetPosition(1);
 
 
-    if(Input.GetMouseButton(0)){
-        lineRenderer.SetPosition(0, new Vector3(linePos.x, linePos.y - (fishingLineSpeed * Time.deltaTime), linePos.z));
-        //Move Camera
-        if(linePos.y < -4){
-            if(camera.transform.position.y > linePos.y){
-                camera.transform.position = new Vector3(camera.transform.position.x, camera.transform.position.y - (fishingLineSpeed * cameraCatchUpSpeed * Time.deltaTime), camera.transform.position.z);
+        if(Input.GetMouseButton(0)){
+            lineRenderer.SetPosition(1, new Vector3(linePos.x, linePos.y - (fishingLineSpeed * Time.deltaTime), linePos.z));
+            //Move Camera
+            if(linePos.y < -4){
+                if(camera.transform.position.y > linePos.y){
+                    camera.transform.position = new Vector3(camera.transform.position.x, camera.transform.position.y - (fishingLineSpeed * cameraCatchUpSpeed * Time.deltaTime), camera.transform.position.z);
+                }
+
+                if(camera.transform.position.y <= linePos.y - gameObject.transform.position.y){
+                    camera.transform.position = new Vector3(camera.transform.position.x, camera.transform.position.y - (fishingLineSpeed * Time.deltaTime), camera.transform.position.z);
+                }
+
+                
             }
-
-            if(camera.transform.position.y <= linePos.y - gameObject.transform.position.y){
-                camera.transform.position = new Vector3(camera.transform.position.x, camera.transform.position.y - (fishingLineSpeed * Time.deltaTime), camera.transform.position.z);
-            }
-
             
         }
-        
-    }
-    else{
-        //Reset fishing line and camera
-        if(linePos.y < 0){
-            lineRenderer.SetPosition(0, new Vector3(linePos.x, linePos.y + (fishingLineSpeed * Time.deltaTime), linePos.z));
+        else{
+            //Reset fishing line and camera
+            if(linePos.y < 0){
+                lineRenderer.SetPosition(1, new Vector3(linePos.x, linePos.y + (fishingLineSpeed * Time.deltaTime), linePos.z));
+            }
+            if(camera.transform.position.y < 0){
+                camera.transform.position = new Vector3(camera.transform.position.x, camera.transform.position.y + (fishingLineSpeed * Time.deltaTime), camera.transform.position.z);
+            }
         }
-        if(camera.transform.position.y < 0){
-            camera.transform.position = new Vector3(camera.transform.position.x, camera.transform.position.y + (fishingLineSpeed * Time.deltaTime), camera.transform.position.z);
+
+    }
+
+    void FixedUpdate()
+    {
+
+        Vector3 origin = lineRenderer.transform.position;
+        Vector3 direction = (linePos - origin).normalized;
+        float maxDistance = (lineRenderer.transform.position.y -  linePos.y) - 2;
+
+        RaycastHit2D hit = Physics2D.Raycast(origin, direction, maxDistance);
+        if (hit.collider != null)
+        {
+            if (hit.collider.GetComponent<BoxCollider2D>().tag == "Fish")
+            {
+                Debug.Log("Hit");
+            }
         }
-    }
 
-
-    void FixedUpdate(){
-
-        // if (Physics.Raycast(linePos, transform.TransformDirection(Vector3.forward), out hit, Mathf.Infinity, layerMask))
-        //     {
-        //         Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * hit.distance, Color.yellow);
-        //         Debug.Log("Did Hit");
-        //     }
-        //     else
-        //     {
-        //         Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * 1000, Color.white);
-        //         Debug.Log("Did not Hit");
-        //     }
-
-
-
-    }
+        Debug.DrawRay(origin, linePos, Color.red);
 
     }
 }
