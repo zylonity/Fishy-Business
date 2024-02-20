c09b71f25f6dabb7f3cc4364c8d3039f499818d3
Melika Khan <120108978+meleyeka@users.noreply.github.com>
2024-02-20 01:44:35 +0000

    Hook movement control with mouse

--- Assets/Scenes/catchBar/fishingMechanic.cs
+++ Assets/Scenes/catchBar/fishingMechanic.cs
@@ -17,19 +17,49 @@ public class fishingMechanic : MonoBehaviour
     float fishSpeed;
     [SerializeField] float smoothMotion = 1f;
 
+    [SerializeField] Transform hook;
+    float hookPosition;
+    [SerializeField] float hookSize = 0.1f;
+    [SerializeField] float hookPower = 0.5f;
+    float hookProgress;
+    float hookPullVelocity;
+    [SerializeField] float hookPullPower = 0.01f;
+    [SerializeField] float hookGravity = 0.005f;
+    [SerializeField] float hookDeProgressPower = 0.1f;
+
+
+
     private void Update()
     {
+        Fish();
+        Hook();
+    }
+
+    void Hook()
+    {
+        if(Input.GetMouseButton(0))
+        {
+            hookPullVelocity += hookPullPower * Time.deltaTime;
+        }
+        hookPullVelocity -= hookGravity * Time.deltaTime;
+
+        hookPosition += hookPullVelocity;
+        hookPosition = Mathf.Clamp(hookPosition, hookSize / 2, 1 - hookSize /2);
+        hook.position = Vector3.Lerp(bottom.position, top.position, hookPosition);
+    }
+
+    void Fish()
+    {
         fishTimer -= Time.deltaTime;
-        if(fishTimer <0f)
+        if (fishTimer < 0f)
         {
             fishTimer = UnityEngine.Random.value;
 
-            fishDestination =  UnityEngine.Random.value;
+            fishDestination = UnityEngine.Random.value;
         }
 
         fishPosition = Mathf.SmoothDamp(fishPosition, fishDestination, ref fishSpeed, smoothMotion);
         fish.position = Vector3.Lerp(bottom.position, top.position, fishPosition);
-
     }
 
 }
