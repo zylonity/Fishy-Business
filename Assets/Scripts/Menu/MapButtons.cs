c89443223cdc5a1674fb1e6171e3493cb0372d3e
Melika Khan <120108978+meleyeka@users.noreply.github.com>
2024-02-28 19:30:39 +0000

    added level locks
    cant proceed onto next level unless the current one has been completed

--- Assets/Scripts/Menu/MapButtons.cs
+++ Assets/Scripts/Menu/MapButtons.cs
@@ -7,7 +7,20 @@ using UnityEngine.UI;
 
 public class MapButtons : MonoBehaviour
 {
-   
+    public Button[] buttons;
+
+    private void Awake()
+    {
+        int unlockedLevel = PlayerPrefs.GetInt("Level");
+        for (int i = 0; i < buttons.Length; i++) 
+        {
+            buttons[i].interactable = false;
+        }
+        for(int i= 0; i < unlockedLevel; i++) 
+        {
+            buttons[i].interactable = true;
+        }
+    }
 
     public void GoToLevel(int sceneID) 
     {
